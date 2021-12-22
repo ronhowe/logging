ceedc11d1117ecc5e6a3ef075acaa326a8b53909
Ron Howe <ronhowe@hotmail.com>
2021-12-22 17:00:04 -0500

    MLP Serilog

--- logging/WebApplication1/Program.cs
+++ logging/WebApplication1/Program.cs
@@ -1,4 +1,16 @@
 using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(outputTemplate: "**ConsoleTemplate** {Timestamp:HH:mm:ss.fff zzz} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}")
+    .CreateLogger();
+
+Log.Debug("**Log.Debug()**");
+Log.Information("**Log.Information()**");
+Log.ForContext("SourceContext", "**SourceContext**").Information("**Log.ForContext().Information()**");
 
 try
 {
@@ -15,14 +27,15 @@ try
 
     app.MapGet("/", () =>
     {
-        app.Logger.LogInformation("**MapGet**");
+        app.Logger.LogDebug("**app.Logger.LogDebug()**");
+        app.Logger.LogInformation("**app.Logger.LogInformation()**");
     });
-
+    throw new NotImplementedException("DIE");
     app.Run();
 }
 catch (Exception ex)
 {
-    Log.Fatal(ex, "WebApplication1 Failed");
+    Log.Fatal(ex, "**Log.Fatal()**");
 }
 finally
 {
